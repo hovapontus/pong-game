13d032c6810de3dfaaf1932ffa617b92bc53c5c5
hovapontus <hovapontus11@gmail.com>
2023-03-22 14:50:17 +0100

    beta

--- Ball.js
+++ Ball.js
@@ -44,7 +44,7 @@ export default class Ball{
         this.velocity = INITIAL_VELOCITY;
     }
 
-    update(delta){
+    update(delta, paddleRects){
         this.x += this.direction.x * this.velocity * delta;
         this.y += this.direction.y * this.velocity * delta;
         this.velocity += VELOCITY_INCREASE * delta;
@@ -53,13 +53,24 @@ export default class Ball{
         if (rect.bottom >= window.innerHeight || rect.top <= 0){
             this.direction.y *= -1;
         }
-        if (rect.bottom >= window.innerWidth || rect.top <= 0){
+
+        if (paddleRects.some(r => isCollision(r, rect))) {
             this.direction.x *= -1;
         }
-
+    
     }
 }
 
 function randomNumberBetween(min, max){
     return Math.random() * (max - min) + min;
+}
+
+
+function isCollision(rect1, rect2){
+    return (
+        rect1.left <= rect2.right &&
+        rect1.right >= rect2.left && 
+        rect1.top <= rect2.bottom && 
+        rect1.bottom >= rect2.top
+    )
 }
--- Paddle.js
+++ Paddle.js
@@ -1,6 +1,10 @@
+const SPEED = .02;
+
+
 export default class Paddle{
     constructor(paddleElem){
         this.paddleElem = paddleElem;
+        this.reset();
     }
 
     get position(){
@@ -9,5 +13,18 @@ export default class Paddle{
 
     set position(value){
         this.paddleElem.style.setProperty("--position", value);
+    }
+
+    rect(){
+        return this.paddleElem.getBoundingClientRect();
+    }
+
+    reset(){
+        this.position = 50;
+    }
+
+    update(delta, ballHeight){
+        this.position += SPEED * delta *(ballHeight - this.position);
+
     }
 }
